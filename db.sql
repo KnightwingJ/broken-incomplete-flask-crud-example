b3390e810848e64d9410690883f71624c5f12810
John Hinch <123460465+KnightwingJ@users.noreply.github.com>
2023-11-29 15:36:00 +0000

    Add files via upload

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ GRANT ALL PRIVILEGES ON *.* to 'A'@'%';
 CREATE DATABASE student; 
 USE student;
 CREATE TABLE students (studentName VARCHAR(255), email VARCHAR(255), studentID INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY(studentID));
+PRIMARY KEY (studentID));
 
 INSERT INTO students (studentName, email) values ("first student", "firststudent@mydomain.ie"); 
 INSERT INTO students (studentName, email) values ("second student", "secondstudent@mydomain.ie ");
